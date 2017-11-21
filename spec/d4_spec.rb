006437b5be76786906b4a55eb4c458c2a834df63
BertZZ <bertie.wooles@hotmail.co.uk>
2017-11-21 16:41:12 +0000

    Changed Testing method

--- spec/d4_spec.rb
+++ spec/d4_spec.rb
@@ -5,8 +5,12 @@ describe D4 do
   subject(:dice) { described_class.new }
   it { is_expected.to be_a D4 }
 
-  it 'rolls a 4 sided dice' do
-    expect(dice).to receive(:rand).and_return(4)
-    expect(dice.roll).to eq 4
+  it 'doesnt roll above 4' do
+    result = dice.roll
+    expect(result).to be <= 4
+  end
+  it 'doesnt roll below 1' do
+    result = dice.roll
+    expect(result).to be >= 1
   end
 end
--- spec/d6_spec.rb
+++ spec/d6_spec.rb
@@ -5,8 +5,12 @@ describe D6 do
   subject(:dice) { described_class.new }
   it { is_expected.to be_a D6 }
 
-  it 'rolls a 6 sided dice' do
-    expect(dice).to receive(:rand).and_return(6)
-    expect(dice.roll).to eq 6
+  it 'doesnt roll above 6' do
+    result = dice.roll
+    expect(result).to be <= 6
+  end
+  it 'doesnt roll below 1' do
+    result = dice.roll
+    expect(result).to be >= 1
   end
 end
